867b08e906f93b72ec25ee026e6839802edfe9b4
El <evriel.leovaztion@gmail.com>
2024-12-09 13:59:48 +0700

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,8 +1,15 @@
 import gradio as gr
 
+<<<<<<< Updated upstream
 def process_image():
     return 
 
 
 demo = gr.Interface(fn=process_image, inputs=gr.Image(type="filepath"), outputs="text")
+=======
+def greet(name):
+    return "Hello " + name + "!"
+
+demo = gr.Interface(fn=greet, inputs=gr.Image(), outputs="text")
+>>>>>>> Stashed changes
 demo.launch()   
